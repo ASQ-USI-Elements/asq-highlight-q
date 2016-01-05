1e448273fa25245e466ed4bd776e5fdf921d718e
Vasileios Triglianos <triglian@gmail.com>
2016-01-05 15:35:30 +0100

    heatmap: refine checks

--- src/Heatmap.js
+++ src/Heatmap.js
@@ -85,11 +85,13 @@ var Heatmap = module.exports = function(options){
         // TODO: consider using the build Range methods like contains
         if(i > range.start.row && i < range.end.row){
           hueWeights[i][j] = hueWeights[i][j] + (1 - hueWeights[i][j]) / n;
-        }else if(i == range.start.row && j >= range.start.column && j < range.end.column
-          || i == range.end.row && j >= range.start.column && j < range.end.column){
+        }else if(i == range.start.row && i == range.end.row && j >= range.start.column && j < range.end.column){
           hueWeights[i][j] = hueWeights[i][j] + (1 - hueWeights[i][j]) / n;
-        }
-        else{
+        }else if(i == range.start.row && i < range.end.row && j >= range.start.column){
+          hueWeights[i][j] = hueWeights[i][j] + (1 - hueWeights[i][j]) / n;
+        }else if(i > range.start.row && i == range.end.row && j < range.end.column){
+          hueWeights[i][j] = hueWeights[i][j] + (1 - hueWeights[i][j]) / n;
+        }else{
           hueWeights[i][j] = hueWeights[i][j] -  hueWeights[i][j] / n;
         }
 
